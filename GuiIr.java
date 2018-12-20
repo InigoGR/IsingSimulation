e8fd061ce25985777add35e20368e1db2006d388
Iñigo <i.glez.ruiz@gmail.com>
2018-12-20 15:55:45 +0100

    Add files via upload
    GuiIr: Main program to open the graphical user interface.
    Exp: Class that creates the objects necessary for the interface, TODOs mark where the prefered classes should be introduced (there are two classes that play the same role, for example H and Re)

---
+++ GuiIr.java
@@ -0,0 +1,16 @@
+package r2ms.gui.modIr;
+
+class GuiIr {
+	
+	
+/*
+ * Main program using the GlobalFrame class
+ */
+		public static void main(String[] args) {
+			
+			Exp frame=new Exp();
+		}
+		
+}
+
+
